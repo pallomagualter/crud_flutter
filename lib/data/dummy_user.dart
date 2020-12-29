7ebdd950109c64d107337b3a60df7c17eaa17aed
Palloma Gualter <pallomagualter@gmail.com>
2020-12-29 11:44:41 -0300

    dummy user

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,51 @@
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Pig',
+    email: 'pig@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/20/11/avatar-1295575_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Ted',
+    email: 'ted@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Toto',
+    email: 'toto@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png',
+  ),
+  '5': const User(
+    id: '5',
+    name: 'Pernalonga',
+    email: 'pernalonga@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png',
+  ),
+  '6': const User(
+    id: 'Jotinha',
+    name: 'Jotinha',
+    email: 'jotinha@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/05/12/23/03/lamb-1388937_960_720.png',
+  ),
+  '7': const User(
+    id: 'Pandinha',
+    name: 'Pandinha',
+    email: 'pandinha@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2013/07/12/19/29/panda-154852_960_720.png',
+  ),
+};
