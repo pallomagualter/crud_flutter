0fa498a9e77bf571c891bc4900b941af9881465d
Palloma Gualter <pallomagualter@gmail.com>
2020-12-29 11:32:20 -0300

    user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,8 +6,10 @@ class User {
   final String email;
   final String avatarUrl;
 
-  this.id,
-  @required this.name,
-  @required this.email,
-  @required this.avatarUrl,
+  const User({
+    this.id,
+    @required this.name,
+    @required this.email,
+    @required this.avatarUrl,
+  });
 }
