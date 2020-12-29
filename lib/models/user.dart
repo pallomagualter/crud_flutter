902553c70824660342f7c6543abcea000c66bf50
Palloma Gualter <pallomagualter@gmail.com>
2020-12-29 11:31:16 -0300

    user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/material.dart';
+
 class User {
   final String id;
   final String name;
   final String email;
   final String avatarUrl;
+
+  this.id,
+  @required this.name,
+  @required this.email,
+  @required this.avatarUrl,
 }
