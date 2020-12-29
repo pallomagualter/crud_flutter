680e2c2c5a96a71dc90624489f5e43fd2e76fce3
Palloma Gualter <pallomagualter@gmail.com>
2020-12-29 17:27:30 -0300

    user list

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,13 +1,19 @@
-import 'package:flutter/cupertino.dart';
-
+import 'package:cadastro/data/dummy_user.dart';
 import 'package:flutter/material.dart';
+import 'package: crud_flutter/data/dummy_user.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const users = {...DUMMY_USERS};
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuários'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: null,
       ),
     );
   }
